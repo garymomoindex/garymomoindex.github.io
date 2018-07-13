79b673b61ca3165db9f5a3636cc1cb4fa0a9ea83
garymomoindex <garymomoindex@gmail.com>
2018-07-13 09:24:23 +0800

    2018/07/13 增加連結與修改資源路徑

--- js/general/custom/head.js
+++ js/general/custom/head.js
@@ -1,9 +1,14 @@
-﻿// CSS
+﻿// FONT
+document.writeln("<link href='fonts/font.css' rel='stylesheet'>");
+
+// CSS
 // jquery ui
-document.writeln("<link rel='stylesheet' href='https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css' />");
+document.writeln("<link rel='stylesheet' href='css/jquery-ui.min.css' />");
+document.writeln("<link rel='stylesheet' href='css/jquery-ui.structure.min.css' />");
+document.writeln("<link rel='stylesheet' href='css/jquery-ui.theme.min.css' />");
 // bootstrap
-document.writeln("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css' />");
-document.writeln("<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css' />");
+document.writeln("<link rel='stylesheet' href='css/bootstrap.min.css' />");
+document.writeln("<link rel='stylesheet' href='css/bootstrap-theme.min.css' />");
 //custom
 document.writeln("<link rel='stylesheet' href='css/general/custom/style.css' />");
 document.writeln("<link rel='stylesheet' href='css/general/custom/nav.css' />");
@@ -11,20 +16,17 @@ document.writeln("<link rel='stylesheet' href='css/general/custom/indexVideo.css
 document.writeln("<link rel='stylesheet' href='css/general/custom/yearGear.css' />");
 document.writeln("<link rel='stylesheet' href='css/general/custom/foot.css' />");
 
-// FONT
-document.writeln("<link href='https://fonts.googleapis.com/css?family=Wendy+One' rel='stylesheet'>");
-
 // JS
 // jquery
-document.writeln("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js'></script>");
+document.writeln("<script src='js/jquery-3.3.1.min.js'></script>");
 // jquery ui
-document.writeln("<script src='https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js'></script>");
+document.writeln("<script src='js/jquery-ui.min.js'></script>");
 // bootstrap
-document.writeln("<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js'></script>");
+document.writeln("<script src='js/bootstrap.min.js'></script>");
 // GSAP
-document.writeln("<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/TweenMax.min.js'></script>");
-document.writeln("<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/plugins/TextPlugin.min.js'></script>");
-document.writeln("<script src='https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.4/utils/Draggable.min.js'></script>");
+document.writeln("<script src='js/general/TweenMax.min.js'></script>");
+document.writeln("<script src='js/general/TextPlugin.min.js'></script>");
+document.writeln("<script src='js/general/Draggable.min.js'></script>");
 document.writeln("<script src='js/general/Physics2DPlugin.min.js'></script>");
 document.writeln("<script src='js/general/DrawSVGPlugin.min.js'></script>");
 document.writeln("<script src='js/general/ThrowPropsPlugin.min.js'></script>");
