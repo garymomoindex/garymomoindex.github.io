cadc860b5136f586e315fa02003a9b2de90c1fef
garymomoindex <garymomoindex@gmail.com>
2018-08-19 03:50:45 +0800

    2018/08/19 動畫測試

--- js/general/custom/main-link.js
+++ js/general/custom/main-link.js
@@ -2,6 +2,8 @@
 	var width = window.innerWidth;
 	var ori_width = "0";
 	var setheight = "0";
+	var linkDownOffset = 50;
+	var linkShowId = "";
 	var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
 
 	resize();
@@ -28,21 +30,39 @@
 			}
 		}
 		ori_width = width;
+
+		if (linkShowId != "" && linkShowId != null) {
+			var itemTop = $("#" + linkShowId).position().top;
+			var itemOHeight = $("#" + linkShowId).outerHeight(true);
+			var footerTop = $("footer").position().top;
+			var linkDownTop = (itemTop + itemOHeight) + linkDownOffset;
+			$(".link-info-down").css("top", linkDownTop + "px");
+			$(".link-info-down").css("height", (footerTop - linkDownTop - linkDownOffset) + "px");
+		}
 	}
 
-//	$(".link-items").on("mouseenter taphold", function() {
-//		$(".link-info-up").show();
-//		$(".link-info-down").show();
-//	});
-//
-//	$(".link-items").on("mouseleave taphold", function() {
-//		$(".link-info-up").hide();
-//		$(".link-info-down").hide();
-//	});
+	$(".link-items").on("click tap", function() {
+		var itemTop = $(this).position().top;console.log(itemTop);
+		var linkUpHeight =  parseInt($(".link-info-up").css("height").substring(0, $(".link-info-up").css("height").indexOf("px")));
+		$(".link-info-up").css(
+			"height", (linkUpHeight + itemTop) + "px"
+		);
+		$("#link-info-up-title").text("測試上");
 
-	$(".link-items").on("swipe", function() {
+		$(".link-info-up-base").show();
 		$(".link-info-up").show();
+
+		itemTop = $(this).position().top;
+		var itemOHeight = $(this).outerHeight(true);
+		var footerTop = $("footer").position().top;
+		var linkDownTop = (itemTop + itemOHeight) + linkDownOffset;
+		$(".link-info-down").css("top", linkDownTop + "px");
+		$(".link-info-down").css("height", (footerTop - linkDownTop) + "px");
+		$("#link-info-down-title").text("測試下");
+
 		$(".link-info-down").show();
-		console.log("123");
+		$(".link-info-down-base").show();
+
+		linkShowId = $(this).attr("id");
 	});
 });
