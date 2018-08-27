1c8290303a452586d7a0bbf297f1de1bf2ede7a3
garymomoindex <garymomoindex@gmail.com>
2018-08-27 21:49:38 +0800

    2018/08/27 「關於」頁面架構

---
+++ js/general/custom/main-about.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function() {
+	var width = window.innerWidth;
+	var ori_width = "0";
+	var setheight = "0";
+	var animationEnd = "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+
+	resize();
+	$(window).on("resize", resize);
+
+	function resize() {
+		width = window.innerWidth;
+
+		$(".mainContain").css("height","auto");
+		if($(window).height() > $(".mainContain").css("height").substring(0, $(".mainContain").css("height").indexOf("px"))) {
+			setheight = $(window).height();
+			$(".mainContain").css("height", setheight);
+		}
+
+		if (width != ori_width) {
+			if (width > 767) {
+				$("#yearList").css("display", "none");
+				$(".container-fluid").css("padding-right", "0px");
+				$(".container-fluid").css("padding-left", "0px");
+			} else {
+				$("#yearList").css("display", "");
+				$(".container-fluid").css("padding-right", "15px");
+				$(".container-fluid").css("padding-left", "15px");
+			}
+			$("#about-img").outerHeight($("#about-img").outerWidth());
+		}
+		ori_width = width;
+	}
+});
