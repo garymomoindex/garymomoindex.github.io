fb962fe8c80cd2512235f09e85123c39c3916f69
garymomoindex <garymomoindex@gmail.com>
2019-07-07 23:05:30 +0800

    2019/7/7 2019標題決定

--- js/general/custom/nav-full-member.js
+++ js/general/custom/nav-full-member.js
@@ -14,7 +14,7 @@ document.writeln("			<ul class='nav navbar-nav'>");
 document.writeln("				<li class='dropdown'>");
 document.writeln("					<a class='dropdown-toggle' data-toggle='dropdown'>穿越 -時-<span class='caret'></span></a>");
 document.writeln("					<ul class='dropdown-menu' id='momoNavTime'>");
-document.writeln("						<li><a href='https://garymomo2019.github.io/'>2019 -未定-</a></li>");
+document.writeln("						<li><a href='https://garymomo2019.github.io/'>2019 -鑽石意志-</a></li>");
 document.writeln("						<li><a href='https://garymomo2018.github.io/'>2018 -守護四葉-</a></li>");
 document.writeln("						<li><a href='https://garymomo2017.github.io/'>2017 -綠意青春-</a></li>");
 //document.writeln("						<li><a href='https://garymomo2016.github.io/redirection/'>2016 -自由獨行-</a></li>");
